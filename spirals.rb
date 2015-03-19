4b2fbb6f84dfb95476a4c18c956befa5b4177cb0
joshBadger <badger.joshua@gmail.com>
2015-03-19 15:08:13 -0600

    added guard clause

--- spirals.rb
+++ spirals.rb
@@ -7,38 +7,42 @@ def spirals(start, width)
   right_bound = width - 1
   number = start
 
-  unless(width == 0)
-    while(upper_bound <= width / 2)
-      (left_bound..right_bound).each do |i|
-        result[upper_bound][i] = number
+  increment = ->(first_bound, second_bound){
+
+  }
+
+  return [] if width == 0
+  
+  while(upper_bound <= width / 2)
+    (left_bound..right_bound).each do |i|
+      result[upper_bound][i] = number
+      number += 1
+    end
+
+    unless(number == width * width + number - 1)
+      upper_bound += 1
+
+      (upper_bound..bottom_bound).each do |i|
+        result[i][right_bound] = number
         number += 1
       end
 
-      unless(number == width * width + number - 1)
-        upper_bound += 1
+      right_bound -= 1
 
-        (upper_bound..bottom_bound).each do |i|
-          result[i][right_bound] = number
-          number += 1
-        end
-
-        right_bound -= 1
-
-        right_bound.downto(left_bound) do |i|
-          result[bottom_bound][i] = number
-          number += 1
-        end
-
-        bottom_bound -= 1
-
-        bottom_bound.downto(upper_bound) do |i|
-          result[i][left_bound] = number
-          number += 1
-        end
+      right_bound.downto(left_bound) do |i|
+        result[bottom_bound][i] = number
+        number += 1
       end
 
-      left_bound += 1
-    end #while
-  end #unless
-  return result
+      bottom_bound -= 1
+
+      bottom_bound.downto(upper_bound) do |i|
+        result[i][left_bound] = number
+        number += 1
+      end
+    end
+
+    left_bound += 1
+  end #while
+  result
 end #def
